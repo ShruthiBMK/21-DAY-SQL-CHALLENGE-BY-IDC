261f1ac8135a9549e40270b2ef5f5153ac45d34a
Shruthi Bhaskaran M K <srubusinessutube@gmail.com>
2025-11-14 20:38:05 -0500

    Add files via upload

---
+++ DAY 12.sql
@@ -0,0 +1,28 @@
+-- ----------------------------------------------------------DAY 12---------------------------------------------------------------------
+-- Practice questions
+-- 1. Find all weeks in services_weekly where no special event occurred.
+select distinct week FROM services_weekly 
+WHERE event = 'none';
+-- 2. Count how many records have null or empty event values.
+SELECT COUNT(*) FROM services_weekly 
+WHERE event = 'none';
+-- 3. List all services that had at least one week with a special event.
+SELECT DISTINCT service
+FROM services_weekly
+WHERE event IS NOT NULL 
+  AND event <> 'none';
+
+-- DAY 12 challenge
+-- Analyze the event impact by comparing weeks with events vs weeks without events. Show: event status ('With Event' or 'No Event'),
+ -- count of weeks, average patient satisfaction, and average staff morale.  Order by average patient satisfaction descending.
+ SELECT service,
+	CASE
+    WHEN event = "none" THEN 'No event'
+	ELSE 'With event' 
+    END AS event_status,
+   COUNT(DISTINCT week) AS no_of_weeks,
+   ROUND(AVG(patients_satisfaction),2) AS avg_patient_satisfaction,
+   ROUND(AVG(staff_morale),2) AS avg_staff_morale
+FROM services_weekly
+GROUP BY service, event_status
+ORDER BY avg_patient_satisfaction DESC;
